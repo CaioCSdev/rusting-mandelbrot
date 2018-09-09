01c4054a5f2af43c6620dee728443b5ec3ce59e6
Caio Salgado <caio.csalgado@gmail.com>
2018-09-09 04:26:04 -0300

    At this moment it does not compile, vut when uncomment line 27or29 it do

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 extern crate num;
+extern crate image;
+
+use image::ColorType;
+use image::png::PNGEncoder;
 use num::Complex;
 use std::str::FromStr;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGTH").unwrap();
+        writeln!(std::io::stderr(), "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0])
+            .unwrap();
+
+        std::process::exit(1);
+    }
+    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("Error parsing image dimensions");
+    let lower_right = parse_complex(&args[4]).expect("Error parsing image dimensions");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    // render(&mut pixels, bounds, upper_left, lower_right);
+
+    // write_image(&args[1], &pixels, bounds).expect("Error writing PNG file")
 }
 
 fn pixel_to_point(
@@ -11,8 +34,7 @@ fn pixel_to_point(
                   pixel: (usize, usize),
                   upper_left: Complex<f64>,
                   lower_right: Complex<f64>
-    ) -> Complex<f64>
-{
+    ) -> Complex<f64> {
     let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
 
     Complex {
@@ -54,6 +76,7 @@ fn test_parse_complex() {
     assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-0.0625"), None);
 }
+
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator){
         None => None,
@@ -77,6 +100,25 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x1.5",   'x'), Some((0.5, 1.5)));
 }
 
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>
+    ){
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[ row * bounds.0 + column ] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+}
+
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
 
@@ -87,4 +129,13 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
         }
     }
     None
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
